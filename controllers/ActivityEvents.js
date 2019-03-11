7ce95609e9a1b5cb91e6ae67bedfb43fb090d1bc
f4ntal3mon <45206581+f4ntal3mon@users.noreply.github.com>
2019-03-11 21:25:02 +0000

    Added CORS headers to all endpoints

--- controllers/ActivityEvents.js
+++ controllers/ActivityEvents.js
@@ -6,36 +6,20 @@ var ActivityEvents = require('../service/ActivityEventsService');
 module.exports.deleteActivitiesActivityid = function deleteActivitiesActivityid (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.deleteEventActivityMappingsEventActivityMappingid = function deleteEventActivityMappingsEventActivityMappingid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.deleteEventSubjectMappingsEventSubjectMappingid = function deleteEventSubjectMappingsEventSubjectMappingid (req, res, next) {
-  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
-  res.end();
-};
-
-module.exports.deleteEventsEventid = function deleteEventsEventid (req, res, next) {
-  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
-  res.end();
-};
-
-module.exports.deleteObservationsObservationid = function deleteObservationsObservationid (req, res, next) {
-  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
-  res.end();
-};
-
-module.exports.deleteSubjectsSubjectid = function deleteSubjectsSubjectid (req, res, next) {
-  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
-  res.end();
-};
-
-module.exports.getActivities = function getActivities (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
@@ -43,43 +27,98 @@ module.exports.getActivities = function getActivities (req, res, next) {
   res.end();
 };
 
+module.exports.deleteEventsEventid = function deleteEventsEventid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
+  res.end();
+};
+
+module.exports.deleteObservationsObservationid = function deleteObservationsObservationid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
+  res.end();
+};
+
+module.exports.deleteSubjectsSubjectid = function deleteSubjectsSubjectid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
+  res.end();
+};
+
+module.exports.getActivities = function getActivities (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
+  res.end();
+};
+
 module.exports.getActivitiesActivityid = function getActivitiesActivityid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEventActivityMappings = function getEventActivityMappings (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEventActivityMappingsEventActivityMappingid = function getEventActivityMappingsEventActivityMappingid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEventSubjectMappings = function getEventSubjectMappings (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEventSubjectMappingsEventSubjectMappingid = function getEventSubjectMappingsEventSubjectMappingid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEvents = function getEvents (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getEventsEventid = function getEventsEventid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getObservations = function getObservations (req, res, next) {
-  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
   
@@ -104,7 +143,9 @@ module.exports.getObservations = function getObservations (req, res, next) {
 };
 
 module.exports.getObservationsObservationid = function getObservationsObservationid (req, res, next) {
-  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
   
@@ -121,71 +162,113 @@ module.exports.getObservationsObservationid = function getObservationsObservatio
 };
 
 module.exports.getSubjects = function getSubjects (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.getSubjectsSubjectid = function getSubjectsSubjectid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postActivities = function postActivities (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postEventActivityMappings = function postEventActivityMappings (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postEventSubjectMappings = function postEventSubjectMappings (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postEvents = function postEvents (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postObservations = function postObservations (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.postSubjects = function postSubjects (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putActivitiesActivityid = function putActivitiesActivityid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putEventActivityMappingsEventActivityMappingid = function putEventActivityMappingsEventActivityMappingid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putEventSubjectMappingsEventSubjectMappingid = function putEventSubjectMappingsEventSubjectMappingid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putEventsEventid = function putEventsEventid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putObservationsObservationid = function putObservationsObservationid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
 
 module.exports.putSubjectsSubjectid = function putSubjectsSubjectid (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.writeHead(307, {'Location': 'https://activityevents.restlet.net' + req.url});
   res.end();
 };
